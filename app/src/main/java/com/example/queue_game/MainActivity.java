9c1385cb53c7ac71cc4b9835606f84475b446713
billywu94 <billy.wu02@myhunter.cuny.edu>
2017-04-28 17:34:12 -0400

    added constraints

--- app/src/main/java/com/example/queue_game/MainActivity.java
+++ app/src/main/java/com/example/queue_game/MainActivity.java
@@ -7,6 +7,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
 
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -17,12 +18,10 @@ public class MainActivity extends AppCompatActivity {
     private Random rand = new Random();
     private Character boxLetter; // the letter that will show up in the center of the screen
     private int score;
-    int amountA=0;
-    int amountB=0;
-    int amountC=0;
-    int amountD=0;
-
-
+    int amountA = 0;
+    int amountB = 0;
+    int amountC = 0;
+    int amountD = 0;
 
 
     @Override
@@ -32,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    protected void onStart(View){
+
+    protected void onStart(View view) {
 
         long secondCounter = System.currentTimeMillis() * 1000; //returns the current millseconds
         while (secondCounter < (secondCounter * 5000)) { //to be less then 5 seconds
@@ -44,54 +44,54 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    public void letterCreator(){
-            int letterPicker= rand.nextInt(4)+1;
-            switch (letterPicker){
-                case 1 :
-                    boxLetter = 'a';
-                    break;
-                case 2 :
-                    boxLetter = 'b';
-                    break;
-                case 3 :
-                    boxLetter = 'c';
-                    break;
-                case 4 :
-                    boxLetter = 'd';
-                    break;
-                default:
-                    System.out.println("Didn't work");
-            }
+
+    public void letterCreator() {
+        int letterPicker = rand.nextInt(4) + 1;
+        switch (letterPicker) {
+            case 1:
+                boxLetter = 'a';
+                break;
+            case 2:
+                boxLetter = 'b';
+                break;
+            case 3:
+                boxLetter = 'c';
+                break;
+            case 4:
+                boxLetter = 'd';
+                break;
+            default:
+                System.out.println("Didn't work");
+        }
 
 
     }
 
 
-
     public void enqueue() {
-        if (this.queue.size() == 5){
-            break;
-        } else if (this.queue.size() == 0){
+        if (this.queue.size() == 5) {
+            System.out.println("size is 5");
+        } else if (this.queue.size() == 0) {
             queue.add(boxLetter);
             this.setScore(boxLetter);
             TextView textView = (TextView) findViewById(R.id.q1);
             textView.setText(boxLetter);
-        } else if (this.queue.size() == 1){
+        } else if (this.queue.size() == 1) {
             queue.add(boxLetter);
             this.setScore(boxLetter);
             TextView textView = (TextView) findViewById(R.id.q2);
             textView.setText(boxLetter);
-        } else if (this.queue.size() == 2){
+        } else if (this.queue.size() == 2) {
             queue.add(boxLetter);
             this.setScore(boxLetter);
             TextView textView = (TextView) findViewById(R.id.q3);
             textView.setText(boxLetter);
-        } else if (this.queue.size() == 3){
+        } else if (this.queue.size() == 3) {
             queue.add(boxLetter);
             this.setScore(boxLetter);
             TextView textView = (TextView) findViewById(R.id.q4);
             textView.setText(boxLetter);
-        } else if (this.queue.size() == 4){
+        } else if (this.queue.size() == 4) {
             queue.add(boxLetter);
             this.setScore(boxLetter);
             TextView textView = (TextView) findViewById(R.id.q5);
@@ -99,17 +99,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
     public void dequeue() {
-        if (this.queue.size()==1){
+        if (this.queue.size() == 1) {
             //remove elemnt
-        } else if (this.queue.size()== 2)  {
+        } else if (this.queue.size() == 2) {
             this.queue.remove();
             TextView textView = (TextView) findViewById(R.id.q1);
             textView.setText("");
             textView.setText(queue.peek());
             TextView nextView = (TextView) findViewById(R.id.q2);
             nextView.setText("");
-        } else if (this.queue.size()== 3){
+        } else if (this.queue.size() == 3) {
             this.queue.remove();
             TextView textView = (TextView) findViewById(R.id.q1);
             textView.setText("");
@@ -118,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
             TextView thirdView = (TextView) findViewById(R.id.q3);
             secondView.setText(thirdView.getText());
             thirdView.setText("");
-        } else if (this.queue.size()== 4){
+        } else if (this.queue.size() == 4) {
             this.queue.remove();
             TextView textView = (TextView) findViewById(R.id.q1);
             textView.setText("");
@@ -146,42 +147,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-}
     public void setScore(Character fromBox) {
-        if (fromBox=='a'){
+        if (fromBox == 'a') {
             amountA++;
-            if (amountA == 3){
+            if (amountA == 3) {
                 score = score * 2;
-            } else if (amountA > 3){
+            } else if (amountA > 3) {
                 score = 0;
             } else {
                 score++;
             }
-        } else if (){
-            if (boxLetter=='b'){
+        } else if(boxLetter == 'b') {
                 amountB++;
-                if (amountB == 3){
+                if (amountB == 3) {
                     score = score * 2;
-                } else if (amountB > 3){
+                } else if (amountB > 3) {
                     score = 0;
                 } else {
                     score++;
                 }
-            }
-        } else if (boxLetter=='c'){
+            } else if (boxLetter == 'c') {
             amountC++;
-            if (amountC == 3){
+            if (amountC == 3) {
                 score = score * 2;
-            } else if (amountC > 3){
+            } else if (amountC > 3) {
                 score = 0;
             } else {
                 score++;
             }
         } else {
             amountD++;
-            if (amountD == 3){
+            if (amountD == 3) {
                 score = score * 2;
-            } else if (amountD > 3){
+            } else if (amountD > 3) {
                 score = 0;
             } else {
                 score++;
@@ -189,4 +187,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+}
