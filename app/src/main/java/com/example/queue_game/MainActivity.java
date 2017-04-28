dfa629475e685361254562f4e017c80ed036fe3e
Lily Caplan <lrcgal@gmail.com>
2017-04-28 14:17:46 -0400

    more stuff

--- app/src/main/java/com/example/queue_game/MainActivity.java
+++ app/src/main/java/com/example/queue_game/MainActivity.java
@@ -29,28 +29,27 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    protected void onStart(View){
+    protected void onStart(View) {
 
         long secondCounter = System.currentTimeMillis() * 1000; //returns the current millseconds
-        while (secondCounter <(secondCounter*5000)){ //to be less then 5 seconds
-           this.letterCreator();
+        while (secondCounter < (secondCounter * 5000)) { //to be less then 5 seconds
+            this.letterCreator();
             TextView textView = (TextView) findViewById(R.id.main);
             textView.setText(boxLetter);
             //pressing enqueue goes into the queue
             if () {
 
-            } else if (){
+            } else if () {
                 //pressing dequeque removes and shift
             }
 
+            
+
         }
-
-
         // chachter pops up in text view
         // press enqueue to enqueue that letter  into the queue text box
         // or press dequeue to dequeue letter from begining of queue
         //if user has dequeued shift the visuals for the queue elemnts over
-
 
     }
     public void letterCreator(){
@@ -73,9 +72,9 @@ public class MainActivity extends AppCompatActivity {
             }
 
 
-        }
-
     }
+
+
 
     public void enqueue() {
         if (this.queue.size() == 5){
@@ -86,14 +85,29 @@ public class MainActivity extends AppCompatActivity {
             textView.setText(boxLetter);
         } else if (size == 1){
             queue.add(boxLetter);
-            TextView textView = (TextView) findViewById(R.id.q1);
+            TextView textView = (TextView) findViewById(R.id.q2);
             textView.setText(boxLetter);
-            }
-
+        } else if (size == 2){
+            queue.add(boxLetter);
+            TextView textView = (TextView) findViewById(R.id.q3);
+            textView.setText(boxLetter);
+        } else if (size == 3){
+            queue.add(boxLetter);
+            TextView textView = (TextView) findViewById(R.id.q4);
+            textView.setText(boxLetter);
+        } else if (size == 4){
+            queue.add(boxLetter);
+            TextView textView = (TextView) findViewById(R.id.q5);
+            textView.setText(boxLetter);
         }
 
-
-
+    }
+    public void dequeue() {
+        if (this.queue.size()==1){
+            //remove elemnt
+        } else {
+            //remove element and shift
+        }
     }
 
 
