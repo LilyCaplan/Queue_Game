335ae3e58986008f0cd8eff878d2062a8dabf1a6
Lily Caplan <lrcgal@gmail.com>
2017-04-28 14:12:13 -0400

    latest update

--- app/src/main/java/com/example/queue_game/MainActivity.java
+++ app/src/main/java/com/example/queue_game/MainActivity.java
@@ -7,12 +7,19 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
 
+import android.widget.Button;
+import android.widget.TextView;
+
+
 
 public class MainActivity extends AppCompatActivity {
     private Queue<Character> queue = new LinkedList<>();
     private Random rand = new Random();
     private Character boxLetter; // the letter that will show up in the center of the screen
     private int score;
+    Button en = (Button) findViewById(R.id.enqueue);
+    Button de = (Button) findViewById(R.id.dequeue);
+
 
 
     @Override
@@ -26,6 +33,15 @@ public class MainActivity extends AppCompatActivity {
 
         long secondCounter = System.currentTimeMillis() * 1000; //returns the current millseconds
         while (secondCounter <(secondCounter*5000)){ //to be less then 5 seconds
+           this.letterCreator();
+            TextView textView = (TextView) findViewById(R.id.main);
+            textView.setText(boxLetter);
+            //pressing enqueue goes into the queue
+            if () {
+
+            } else if (){
+                //pressing dequeque removes and shift
+            }
 
         }
 
@@ -60,4 +76,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    public void enqueue() {
+        if (this.queue.size() == 5){
+            break;
+        } else if (size == 0){
+            queue.add(boxLetter);
+            TextView textView = (TextView) findViewById(R.id.q1);
+            textView.setText(boxLetter);
+        } else if (size == 1){
+            queue.add(boxLetter);
+            TextView textView = (TextView) findViewById(R.id.q1);
+            textView.setText(boxLetter);
+            }
+
+        }
+
+
+
+    }
+
+
 }
